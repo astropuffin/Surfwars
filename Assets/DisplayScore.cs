5c9d9cd672a8e6f87cf760422f2e94629598f5f7
astropuffin <astropuffin@duadikos.com>
2017-01-22 14:35:47 -0800

    things

---
+++ Assets/DisplayScore.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayScore : MonoBehaviour
+{
+
+    public ScoreKeeper score;
+    public GameObject[] numbers;
+    public bool left;
+
+    // Use this for initialization
+    void Start()
+    {
+        score = FindObjectOfType<ScoreKeeper>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int scoreValue;
+
+        if( left )
+        {
+            scoreValue = score.playerOneScore;
+        }
+        else
+        {
+            scoreValue = score.playerTwoScore;
+        }
+
+        for(int i = 0; i < numbers.Length; i ++)
+        {
+            if( i == scoreValue)
+            {
+                numbers[i].SetActive(true);
+            }
+            else
+            {
+                numbers[i].SetActive(false);
+            }
+        }
+
+    }
+}
---
+++ Assets/ScoreKeeper.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScoreKeeper : MonoBehaviour {
+	public int playerOneScore = 0;
+	public int playerTwoScore = 0;
+	public int maxScore = 1;
+
+	// Use this for initialization
+	void Start () {
+        SceneManager.LoadScene("Dudes", LoadSceneMode.Additive);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void IncrementPlayerScore(bool firstPlayer)
+	{
+		if (firstPlayer)
+			playerOneScore++;
+		else
+			playerTwoScore++;
+
+		if (DidPlayerWin (firstPlayer)) {
+			StartCoroutine (StartGameOver (5));
+		}
+	}
+
+	public bool DidPlayerWin(bool leftPlayer)
+	{
+		if (leftPlayer) {
+			if (playerOneScore >= maxScore)
+				return true;
+		} else {
+			if (playerTwoScore >= maxScore)
+				return true;
+		}
+
+		return false;
+	}
+
+	IEnumerator StartGameOver(int WaitTime)
+	{
+		yield return new WaitForSeconds(WaitTime);
+		playerOneScore = playerTwoScore = 0;
+			
+	}
+}
